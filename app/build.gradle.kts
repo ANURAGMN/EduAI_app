2eca8b7d58cc70289c987b264a4ed26917b932be
Rutika Shelar <shelarutika21@gmail.com>
2026-01-07 17:43:01 +0530

    Added retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,8 @@ android {
         }
         fun prop(name: String, default: String = ""): String = localProps.getProperty(name, default)
         buildConfigField("String", "AUTH_KEY", "\"${prop("AUTH_KEY")}\"")
+        buildConfigField("String", "AGENTIC_AI_BASE_URL", "\"${prop("AGENTIC_AI_BASE_URL")}\"")
+
 
     }
     buildFeatures {
@@ -103,4 +105,16 @@ dependencies {
     implementation("androidx.room:room-runtime:2.8.4")
     implementation("androidx.room:room-ktx:2.8.4")
     ksp("androidx.room:room-compiler:2.8.4")
+
+    //Retrofit for networking
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    //okhttp
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    // Kotlin Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
 }
